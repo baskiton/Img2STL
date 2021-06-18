63a771afb2afb69c5926abd246c6906965cb9974
baskiton <baskiton@mail.ru>
2021-06-18 10:52:03 +0700

    v1.3.1 Update Development Status

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     ],
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
